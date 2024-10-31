500bd66ac8accb8094c1e17f583ae740db2fa008
Sergio Ortega <ortegagarrido.sergio@gmail.com>
2024-10-31 11:14:22 +0100

    mongoose

--- demo_express/controllers/products.controller.js
+++ demo_express/controllers/products.controller.js
@@ -15,11 +15,22 @@ const createProduct = async (req, res) => {
     }
 }
 
+// {
+//     "id":3,
+//     "title": "John Hardy Women's Legends Naga Gold & Silver Dragon Station Chain Bracelet 2",
+//     "price": 695,
+//     "description": "From our Legends Collection, the Naga was inspired by the mythical water dragon that protects the ocean's pearl. Wear facing inward to be bestowed with love and abundance, or outward for protection.",
+//     "image": "https://fakestoreapi.com/img/71pWzhdJNwL._AC_UL640_QL65_ML3_.jpg",
+//     "companyName":"La casa de las flores"
+// }
+
 // READ
 const getProduct = async (req, res) => {
         try {
             const id = req.params.id;
-            let products = id? await Product.find({id},'-_id -__v') : await Product.find({},'-_id -__v'); //{}
+            let products = id
+            ? await Product.find({id},'-_id -__v').populate('provider','-_id -__v')
+            : await Product.find({},'-_id -__v').populate('provider','-_id -__v'); //{}
             res.status(200).json(products); // Respuesta de la API para 1 producto
         }
         catch (error) {
